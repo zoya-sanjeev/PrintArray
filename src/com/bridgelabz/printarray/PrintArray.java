648bd9b54e298207e53ed0d3abc6d932aca155dd
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-08-30 14:43:36 +0530

    ADDED: toPrint method for printing arrays of int,double and string

--- src/com/bridgelabz/printarray/PrintArray.java
+++ src/com/bridgelabz/printarray/PrintArray.java
@@ -1,5 +1,36 @@
 package com.bridgelabz.printarray;
 
 public class PrintArray {
+	
+	public static void toPrint(Integer[] inputArray){
+        for(int element : inputArray){
+            System.out.printf("%s", element);
+        }
+        System.out.println();
+    }
+
+    public static void toPrint(Double[] inputArray){
+        for(double element : inputArray){
+            System.out.printf("%s", element);
+        }
+        System.out.println();
+    }
+
+    public static void toPrint(Character[] inputArray){
+        for(char element : inputArray){
+            System.out.printf("%s", element);
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Integer[] integerArray = {1,2,3,4,5};
+        Double[] doubleArray = {1.1, 2.2, 3.3,4.4};
+        Character[] characterArray = {'H','E','L','L','O'};
+
+        PrintArray.toPrint(integerArray);
+        PrintArray.toPrint(doubleArray);
+        PrintArray.toPrint(characterArray);
+    }
 
 }
